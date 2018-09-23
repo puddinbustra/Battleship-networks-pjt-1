f9449146f6a8e60e0a69d2f307e0d1f223614c5c
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2018-09-23 13:16:40 -0600

    Couple changes + update()

--- Server.py
+++ Server.py
@@ -7,9 +7,9 @@ Created on Sep 20, 2018
 
 # first of all import the socket library
 import socket
-#Pie is good
-# next create a socket object
-s = socket.socket()
+
+# next create a socket object, with parameters ip and connection type (tcp)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Socket successfully created")
 
 # reserve a port on your computer in our
@@ -30,14 +30,44 @@ print( "socket is listening")
 
 # a forever loop until we interrupt it or
 # an error occurs
-while True:
+ships = 17
+while ships > 0:
 
 # Establish connection with client.
     c, addr = s.accept()
     print('Got connection from', addr )
 
+    #Receive the data
+    data = conn.recv(64)
+
     # send a thank you message to the client.
     c.send('Thank you for connecting')
 
-    # Close the connection with the client
-    c.close()
+# Close the connection with the client
+#
+c.close()
+
+def update(xcor, ycor):
+    #Access document (need a get?)
+    #change xcor and ycor so that they're read in properly
+    xcor -= 1
+    ycor = len(board[])- ycor
+    #O represents a miss, but previously shot. x represents a previous hit
+    if board[ycor,xcor] == 'x' or board[ycor,xcor] == 'o':
+        #Print error
+    elif board[ycor,xcor] == ' ':
+        #may need to http post this
+        board[ycor,xcor] = 'o'
+        #return miss
+    #If hit
+    else:
+        #May need http post
+        board[ycor][xcor] = 'x'
+        ships -= 1
+        #return hit
+    
+    #Get 
+    #Probably an http post to your side's board
+    return 
+
+
